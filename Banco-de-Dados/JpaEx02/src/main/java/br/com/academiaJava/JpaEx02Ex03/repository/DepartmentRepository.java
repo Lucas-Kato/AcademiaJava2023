dcc7c2dca603e58d83cf4c431ab9d28d84075cb4
Lucas-Kato <118133248+Lucas-Kato@users.noreply.github.com>
2023-05-20 14:58:41 -0300

    Delete Banco-de-Dados/JpaEx02 directory

--- Banco-de-Dados/JpaEx02/src/main/java/br/com/academiaJava/JpaEx02Ex03/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.academiaJava.JpaEx02Ex03.repository;
-
-import br.com.academiaJava.JpaEx02Ex03.orm.Department;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends CrudRepository<Department, Integer> {
-}
